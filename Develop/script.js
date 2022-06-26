8e2996b8679e260e3d2402b2c86f583d44791a21
tornicke <torniqe@gmail.com>
2022-06-26 12:56:07 +0200

    Defining numbers variable

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,13 @@
 // Assignment code here
 
-// Listing special characters
+// Defining specialCharacters variable, listing individual special characters
 var specialCharacters = ["!", "”", "#", "$", "%", "&", "`", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", ","];
-// Listing uppercase letters
+// Defining uppercaseLetters variable, listing individual uppercase letters
 var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// Listing lowercase letters 
+// Defining lowercaseLetters variable, listing individual lowercase letters
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-//! Complete the list of special characters
-//! Include "space"? 
-
-//! Repeat this for other character types
+// Defining numbers variable, listing individual numbers
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function generatePassword() {
 var possibleCharacters = []
