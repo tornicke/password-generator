0458d66ebe296f4634f958723c5afc233371d4c7
tornicke <torniqe@gmail.com>
2022-06-26 12:42:19 +0200

    Defining the uppercaseLetters variable

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,15 @@
 // Assignment code here
 
 // Listing special characters
-var specialCharacters = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~",]
+var specialCharacters = ["!", "”", "#", "$", "%", "&", "`", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", ",",
+];
+// Listing uppercase letters
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-// ! Complete the list of special characters
-// ! Include "space"? 
+//! Complete the list of special characters
+//! Include "space"? 
 
-// ! Repeat this for other character types
+//! Repeat this for other character types
 
 function generatePassword() {
 var possibleCharacters = []
@@ -21,7 +24,7 @@ var possibleCharacters = []
     return;
   }
 
-  //Asking users if they want special characters
+  // Asking users if they want special characters
   var allowSpecialCharacters = confirm ("Do you want spec. characters")
   if (allowSpecialCharacters) {
   possibleCharacters = possibleCharacters.concat(specialCharacters)
@@ -33,7 +36,7 @@ var possibleCharacters = []
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-//Write password to the #password input
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
